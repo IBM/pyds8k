ade6e0e5a3f712e5b6c2d78769824bf8afcc0057
Michelle Wu <shwzhang@cn.ibm.com>
2019-11-27 16:18:38 +0800

    Add progamming language python 3.6
    Signed-off-by: Michelle Wu <shwzhang@cn.ibm.com>

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Environment :: Console',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
