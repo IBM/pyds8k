8ab6a46d11aecdc1efdb6854d44232e611c4baf8
ArbelNathan <arbel.nathan@ibm.com>
2020-05-13 17:15:15 +0300

    try to debug

--- pyds8k/httpclient.py
+++ pyds8k/httpclient.py
@@ -207,7 +207,8 @@ class HTTPClient(object):
                     logger.info(REDIRECTING.format(old_url, url))
                     continue
                 elif resp.status_code >= 400:
-                    return resp, {url, method, kwargs, resp}
+                    logger.debug([url, method, kwargs, resp])
+                    return resp, [url, method, kwargs, resp]
                 return resp, body
 
             except exceptions.Unauthorized as e:
