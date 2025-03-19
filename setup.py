398ae4df7094106e3ea97a49d3c261aaa98ccda1
NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
2025-03-19 13:16:08 +0000

    Update requirements and test matrix. (#41)
    * Update requirements.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    * Update actions matrix.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    * Update matrix without sequence.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    * Remove 3.6 for Ubuntu 22.04.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    * Support Python 3.7 minimum.
    Driven by requests.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    * Add Python 3.7 tests on windows-2019.
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: NjM3MjY5NzAgNzA3MzA3 <102704081+NjM3MjY5NzAgNzA3MzA3@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup
 
 import pyds8k
 
-install_requires = ['requests', 'httpretty', 'configparser', 'six']
+install_requires = ['requests', 'configparser', 'six']
 
 setup(
     name='pyds8k',
@@ -31,7 +31,7 @@ setup(
     keywords=["IBM", "DS8000 Storage"],
     requires=install_requires,
     install_requires=install_requires,
-    tests_require=['nose', 'mock'],
+    tests_require=['pytest', 'mock', 'flake8', 'sphinx', 'httpretty'],
     license="Apache License, Version 2.0",
     include_package_data=True,
     packages=find_packages(),
@@ -44,11 +44,12 @@ setup(
         'Environment :: Console',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
